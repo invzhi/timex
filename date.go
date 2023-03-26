8b5028910de38bc79690582ee9b17ef4c017ed39
invzhi <invzhi@protonmail.com>
2023-03-26 22:26:41 +0800

    date: introducing type Date

---
+++ date.go
@@ -0,0 +1,176 @@
+package timex
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type Date struct {
+	year  int
+	month int
+	day   int
+}
+
+// NewDate returns the date corresponding to year, month, and day.
+func NewDate(year, month, day int) (Date, error) {
+	if year < 0 || year > 9999 {
+		return Date{}, errors.New("year is out of range [0,9999]")
+	}
+
+	if month < 1 || month > 12 {
+		return Date{}, errors.New("month is out of range [1,12]")
+	}
+
+	if days := daysInMonth(year, month); day < 1 || day > days {
+		return Date{}, fmt.Errorf("day is out of range [1,%d]", days)
+	}
+
+	return Date{year: year, month: month, day: day}, nil
+}
+
+// MustNewDate is like NewDate but panics if the date cannot be created.
+func MustNewDate(year, month, day int) Date {
+	date, err := NewDate(year, month, day)
+	if err != nil {
+		panic(`timex: NewDate: ` + err.Error())
+	}
+	return date
+}
+
+// FromTime returns the date specified by t.
+func FromTime(t time.Time) Date {
+	year, month, day := t.Date()
+	return Date{year: year, month: int(month), day: day}
+}
+
+// Today returns the current date.
+func Today(locs ...*time.Location) Date {
+	t := time.Now()
+	if len(locs) > 0 {
+		t = t.In(locs[0])
+	}
+	return FromTime(t)
+}
+
+// Year returns the year of specified by d.
+func (d Date) Year() int { return d.year }
+
+// Month returns the month of specified by d.
+func (d Date) Month() int { return d.month }
+
+// Day returns the day of month specified by d.
+func (d Date) Day() int { return d.day }
+
+// DayOfYear returns the day of year specified by d.
+func (d Date) DayOfYear() int {
+	return daysBeforeMonth(d.year, d.month) + d.day
+}
+
+// Weekday returns the day of week specified by d.
+func (d Date) Weekday() time.Weekday {
+	days := daysBeforeYear(d.year) + daysBeforeMonth(d.year, d.month) + d.day
+	return time.Weekday(days % 7)
+}
+
+// ISOWeek returns the ISO 8601 year and week number specified by d.
+func (d Date) ISOWeek() (year, week int) {
+	delta := int(time.Thursday - d.Weekday())
+	if delta == 4 { // Sunday
+		delta = -3
+	}
+
+	thursday := d.Add(0, 0, delta)
+	return thursday.year, (thursday.DayOfYear()-1)/7 + 1
+}
+
+func norm(hi, lo, base int) (int, int) {
+	if lo < 1 {
+		lo += base
+		hi--
+	}
+	if lo > base {
+		lo -= base
+		hi++
+	}
+	return hi, lo
+}
+
+// Add returns the date corresponding to adding the given number of years, months, and days to d.
+func (d Date) Add(years, months, days int) Date {
+	var (
+		year  = d.year + years
+		month = d.month + months
+		day   = d.day + days
+	)
+
+	for month < 1 || month > 12 {
+		year, month = norm(year, month, 12)
+	}
+
+	n := daysInMonth(year, month)
+	for day < 1 || day > n {
+		month, day = norm(month, day, n)
+		if month < 1 || month > 12 {
+			year, month = norm(year, month, 12)
+		}
+		n = daysInMonth(year, month)
+	}
+
+	return Date{year: year, month: month, day: day}
+}
+
+// Before reports whether the date d is before dd.
+func (d Date) Before(dd Date) bool {
+	return d.year < dd.year ||
+		d.year == dd.year && d.month < dd.month ||
+		d.year == dd.year && d.month == dd.month && d.day < dd.day
+}
+
+// After reports whether the date d is after dd.
+func (d Date) After(dd Date) bool {
+	return d.year > dd.year ||
+		d.year == dd.year && d.month > dd.month ||
+		d.year == dd.year && d.month == dd.month && d.day > dd.day
+}
+
+// daysInYear[month] counts the number of days in a non-leap year when the month ends.
+var daysInYear = [...]int32{
+	0,
+	31,
+	31 + 28,
+	31 + 28 + 31,
+	31 + 28 + 31 + 30,
+	31 + 28 + 31 + 30 + 31,
+	31 + 28 + 31 + 30 + 31 + 30,
+	31 + 28 + 31 + 30 + 31 + 30 + 31,
+	31 + 28 + 31 + 30 + 31 + 30 + 31 + 31,
+	31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30,
+	31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31,
+	31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30,
+	31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31,
+}
+
+func isLeap(year int) bool {
+	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
+}
+
+func daysInMonth(year, month int) int {
+	if month == 2 && isLeap(year) {
+		return 29
+	}
+	return int(daysInYear[month] - daysInYear[month-1])
+}
+
+func daysBeforeYear(year int) int {
+	y := year - 1
+	return y*365 + y/4 - y/100 + y/400
+}
+
+func daysBeforeMonth(year, month int) int {
+	days := int(daysInYear[month-1])
+	if month > 2 && isLeap(year) {
+		days++
+	}
+	return days
+}
---
+++ date_test.go
@@ -0,0 +1,215 @@
+package timex
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestNewDateError(t *testing.T) {
+	tests := []struct {
+		year, month, day int
+		errString        string
+	}{
+		{-1, 1, 1, "year is out of range [0,9999]"},
+		{10000, 1, 1, "year is out of range [0,9999]"},
+		{2000, -1, 1, "month is out of range [1,12]"},
+		{2000, 0, 1, "month is out of range [1,12]"},
+		{2000, 13, 1, "month is out of range [1,12]"},
+		{2000, 2, -1, "day is out of range [1,29]"},
+		{2000, 2, 30, "day is out of range [1,29]"},
+		{2001, 2, -1, "day is out of range [1,28]"},
+		{2001, 2, 29, "day is out of range [1,28]"},
+	}
+
+	for _, tt := range tests {
+		_, err := NewDate(tt.year, tt.month, tt.day)
+		assert.EqualError(t, err, tt.errString)
+	}
+}
+
+func TestDateFromTime(t *testing.T) {
+	tt := time.Date(1, 1, 1, 0, 0, 0, 0, time.UTC)
+	for i := 0; tt.Year() < 10000; i++ {
+		date := FromTime(tt)
+		assert.Equal(t, tt.Year(), date.Year())
+		assert.Equal(t, int(tt.Month()), date.Month())
+		assert.Equal(t, tt.Day(), date.Day())
+		assert.Equal(t, tt.YearDay(), date.DayOfYear())
+		assert.Equal(t, tt.Weekday(), date.Weekday())
+
+		tt = tt.AddDate(0, 0, 1)
+	}
+}
+
+func TestDateISOWeek(t *testing.T) {
+	tests := []struct {
+		year  int
+		month int
+		day   int
+		y     int
+		w     int
+	}{
+		{1981, 1, 1, 1981, 1},
+		{1982, 1, 1, 1981, 53},
+		{1983, 1, 1, 1982, 52},
+		{1984, 1, 1, 1983, 52},
+		{1985, 1, 1, 1985, 1},
+		{1986, 1, 1, 1986, 1},
+		{1987, 1, 1, 1987, 1},
+		{1988, 1, 1, 1987, 53},
+		{1989, 1, 1, 1988, 52},
+		{1990, 1, 1, 1990, 1},
+		{1991, 1, 1, 1991, 1},
+		{1992, 1, 1, 1992, 1},
+		{1993, 1, 1, 1992, 53},
+		{1994, 1, 1, 1993, 52},
+		{1995, 1, 2, 1995, 1},
+		{1996, 1, 1, 1996, 1},
+		{1996, 1, 7, 1996, 1},
+		{1996, 1, 8, 1996, 2},
+		{1997, 1, 1, 1997, 1},
+		{1998, 1, 1, 1998, 1},
+		{1999, 1, 1, 1998, 53},
+		{2000, 1, 1, 1999, 52},
+		{2001, 1, 1, 2001, 1},
+		{2002, 1, 1, 2002, 1},
+		{2003, 1, 1, 2003, 1},
+		{2004, 1, 1, 2004, 1},
+		{2005, 1, 1, 2004, 53},
+		{2006, 1, 1, 2005, 52},
+		{2007, 1, 1, 2007, 1},
+		{2008, 1, 1, 2008, 1},
+		{2009, 1, 1, 2009, 1},
+		{2010, 1, 1, 2009, 53},
+		{2010, 1, 1, 2009, 53},
+		{2011, 1, 1, 2010, 52},
+		{2011, 1, 2, 2010, 52},
+		{2011, 1, 3, 2011, 1},
+		{2011, 1, 4, 2011, 1},
+		{2011, 1, 5, 2011, 1},
+		{2011, 1, 6, 2011, 1},
+		{2011, 1, 7, 2011, 1},
+		{2011, 1, 8, 2011, 1},
+		{2011, 1, 9, 2011, 1},
+		{2011, 1, 10, 2011, 2},
+		{2011, 1, 11, 2011, 2},
+		{2011, 6, 12, 2011, 23},
+		{2011, 6, 13, 2011, 24},
+		{2011, 12, 25, 2011, 51},
+		{2011, 12, 26, 2011, 52},
+		{2011, 12, 27, 2011, 52},
+		{2011, 12, 28, 2011, 52},
+		{2011, 12, 29, 2011, 52},
+		{2011, 12, 30, 2011, 52},
+		{2011, 12, 31, 2011, 52},
+		{1995, 1, 1, 1994, 52},
+		{2012, 1, 1, 2011, 52},
+		{2012, 1, 2, 2012, 1},
+		{2012, 1, 8, 2012, 1},
+		{2012, 1, 9, 2012, 2},
+		{2012, 12, 23, 2012, 51},
+		{2012, 12, 24, 2012, 52},
+		{2012, 12, 30, 2012, 52},
+		{2012, 12, 31, 2013, 1},
+		{2013, 1, 1, 2013, 1},
+		{2013, 1, 6, 2013, 1},
+		{2013, 1, 7, 2013, 2},
+		{2013, 12, 22, 2013, 51},
+		{2013, 12, 23, 2013, 52},
+		{2013, 12, 29, 2013, 52},
+		{2013, 12, 30, 2014, 1},
+		{2014, 1, 1, 2014, 1},
+		{2014, 1, 5, 2014, 1},
+		{2014, 1, 6, 2014, 2},
+		{2015, 1, 1, 2015, 1},
+		{2016, 1, 1, 2015, 53},
+		{2017, 1, 1, 2016, 52},
+		{2018, 1, 1, 2018, 1},
+		{2019, 1, 1, 2019, 1},
+		{2020, 1, 1, 2020, 1},
+		{2021, 1, 1, 2020, 53},
+		{2022, 1, 1, 2021, 52},
+		{2023, 1, 1, 2022, 52},
+		{2024, 1, 1, 2024, 1},
+		{2025, 1, 1, 2025, 1},
+		{2026, 1, 1, 2026, 1},
+		{2027, 1, 1, 2026, 53},
+		{2028, 1, 1, 2027, 52},
+		{2029, 1, 1, 2029, 1},
+		{2030, 1, 1, 2030, 1},
+		{2031, 1, 1, 2031, 1},
+		{2032, 1, 1, 2032, 1},
+		{2033, 1, 1, 2032, 53},
+		{2034, 1, 1, 2033, 52},
+		{2035, 1, 1, 2035, 1},
+		{2036, 1, 1, 2036, 1},
+		{2037, 1, 1, 2037, 1},
+		{2038, 1, 1, 2037, 53},
+		{2039, 1, 1, 2038, 52},
+		{2040, 1, 1, 2039, 52},
+	}
+
+	for _, tt := range tests {
+		date, err := NewDate(tt.year, tt.month, tt.day)
+		assert.NoError(t, err)
+
+		y, w := date.ISOWeek()
+		assert.Equal(t, tt.y, y)
+		assert.Equal(t, tt.w, w, tt)
+	}
+
+	// The only real invariant: Jan 04 is in week 1
+	for year := 1950; year < 2100; year++ {
+		date, err := NewDate(year, 1, 4)
+		assert.NoError(t, err)
+
+		y, w := date.ISOWeek()
+		assert.Equal(t, year, y)
+		assert.Equal(t, 1, w)
+	}
+}
+
+func TestDateBeforeAfter(t *testing.T) {
+	tests := []struct {
+		d1, d2        Date
+		before, after bool
+	}{
+		{MustNewDate(2000, 1, 1), MustNewDate(2000, 1, 1), false, false},
+
+		{MustNewDate(2000, 1, 1), MustNewDate(2000, 1, 2), true, false},
+		{MustNewDate(2000, 1, 2), MustNewDate(2000, 1, 1), false, true},
+
+		{MustNewDate(2000, 1, 1), MustNewDate(2000, 2, 1), true, false},
+		{MustNewDate(2000, 2, 1), MustNewDate(2000, 1, 1), false, true},
+
+		{MustNewDate(1999, 12, 31), MustNewDate(2000, 1, 1), true, false},
+		{MustNewDate(2000, 1, 1), MustNewDate(1999, 12, 31), false, true},
+	}
+
+	for _, tt := range tests {
+		assert.Equal(t, tt.before, tt.d1.Before(tt.d2), tt)
+		assert.Equal(t, tt.after, tt.d1.After(tt.d2), tt)
+	}
+}
+
+func TestDateAdd(t *testing.T) {
+	tests := []struct {
+		years, months, days int
+	}{
+		{4, 4, 1},
+		{3, 16, 1},
+		{3, 15, 30},
+		{5, -6, -18 - 30 - 12},
+	}
+
+	d1, err := NewDate(2011, 11, 18)
+	assert.NoError(t, err)
+	d2, err := NewDate(2016, 3, 19)
+	assert.NoError(t, err)
+
+	for _, tt := range tests {
+		d3 := d1.Add(tt.years, tt.months, tt.days)
+		assert.Equal(t, d3, d2)
+	}
+}
