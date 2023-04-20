3e51c917ca1656588d1ec4d1b0a37a069f57b5b8
invzhi <invzhi@protonmail.com>
2023-04-20 23:42:15 +0800

    date: support sql.Scanner and drive.Valuer

---
+++ sql.go
@@ -0,0 +1,52 @@
+package timex
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"time"
+)
+
+// Scan implements the sql.Scanner interface.
+func (d *Date) Scan(value interface{}) (err error) {
+	switch v := value.(type) {
+	case []byte:
+		*d, err = ParseDate(RFC3339, string(v))
+	case string:
+		*d, err = ParseDate(RFC3339, v)
+	case time.Time:
+		*d = DateFromTime(v)
+	default:
+		err = fmt.Errorf("unsupported type %T", value)
+	}
+	return err
+}
+
+// Value implements the driver.Valuer interface.
+func (d Date) Value() (driver.Value, error) {
+	return d.Format(RFC3339), nil
+}
+
+// NullDate represents a Date that may be null.
+// NullDate implements the sql.Scanner interface, so it can be used as a scan destination, similar to sql.NullString.
+type NullDate struct {
+	Date  Date
+	Valid bool // Valid is true if Date is not NULL
+}
+
+// Scan implements the sql.Scanner interface.
+func (d *NullDate) Scan(value interface{}) error {
+	if value == nil {
+		d.Date, d.Valid = Date{}, false
+		return nil
+	}
+	d.Valid = true
+	return d.Date.Scan(value)
+}
+
+// Value implements the driver.Valuer interface.
+func (d NullDate) Value() (driver.Value, error) {
+	if !d.Valid {
+		return nil, nil
+	}
+	return d.Date.Value()
+}
---
+++ sql_test.go
@@ -0,0 +1,85 @@
+package timex
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDateScan(t *testing.T) {
+	tests := []struct {
+		value interface{}
+		s     string
+	}{
+		{[]byte("2006-01-02"), "2006-01-02"},
+		{[]byte("1999-12-24"), "1999-12-24"},
+		{"2006-01-02", "2006-01-02"},
+		{"1999-12-24", "1999-12-24"},
+		{"2006-01-02 00:00:00", "2006-01-02"},
+		{"1999-12-24 00:00:00", "1999-12-24"},
+		{time.Date(2006, 1, 2, 0, 0, 0, 0, time.UTC), "2006-01-02"},
+	}
+
+	for _, tt := range tests {
+		var date Date
+		err := date.Scan(tt.value)
+		assert.NoError(t, err)
+
+		assert.Equal(t, tt.s, date.Format(RFC3339))
+	}
+
+	t.Run("NullDate", func(t *testing.T) {
+		var date NullDate
+		err := date.Scan(nil)
+		assert.NoError(t, err)
+		assert.False(t, date.Valid)
+
+		for _, tt := range tests {
+			err = date.Scan(tt.value)
+			assert.NoError(t, err)
+
+			assert.Equal(t, tt.s, date.Date.Format(RFC3339))
+			assert.True(t, date.Valid)
+		}
+	})
+}
+
+func TestDateScanErrors(t *testing.T) {
+	assert.EqualError(t, new(Date).Scan(nil), "unsupported type <nil>")
+	assert.EqualError(t, new(Date).Scan(uint64(1)), "unsupported type uint64")
+
+	t.Run("NullDate", func(t *testing.T) {
+		assert.EqualError(t, new(NullDate).Scan(uint64(1)), "unsupported type uint64")
+	})
+}
+
+func TestDateValue(t *testing.T) {
+	tests := []struct {
+		date  Date
+		value interface{}
+	}{
+		{Date{}, "0000-12-31"},
+		{MustNewDate(2006, 1, 2), "2006-01-02"},
+		{MustNewDate(1996, 12, 24), "1996-12-24"},
+	}
+
+	for _, tt := range tests {
+		value, err := tt.date.Value()
+		assert.NoError(t, err)
+		assert.Equal(t, tt.value, value)
+	}
+
+	t.Run("NullDate", func(t *testing.T) {
+		value, err := NullDate{}.Value()
+		assert.NoError(t, err)
+		assert.Nil(t, value)
+
+		for _, tt := range tests {
+			date := NullDate{Date: tt.date, Valid: true}
+			value, err := date.Value()
+			assert.NoError(t, err)
+			assert.Equal(t, tt.value, value)
+		}
+	})
+}
