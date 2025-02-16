c4e839d9c6e77877db1bd120e35a32d9514f319a
invzhi <invzhi@protonmail.com>
2025-02-16 20:47:26 +0800

    formatting of TimeOfDay

---
+++ time_of_day_format.go
@@ -0,0 +1,317 @@
+package timex
+
+import "errors"
+
+const (
+	tokenMidday = iota + 1
+	tokenMiddayUppercase
+	token24Hour
+	token24HourTwoDigit
+	token12Hour
+	token12HourTwoDigit
+	tokenMinute
+	tokenMinuteTwoDigit
+	tokenSecond
+	tokenSecondTwoDigit
+)
+
+const (
+	RFC3339Time = "HH:mm:ss"
+)
+
+func nextTimeToken(layout string) (prefix string, token int, suffix string) {
+	for i := 0; i < len(layout); i++ {
+		switch layout[i] {
+		case 'a':
+			return layout[:i], tokenMidday, layout[i+1:]
+		case 'A':
+			return layout[:i], tokenMiddayUppercase, layout[i+1:]
+		case 'H': // H, HH
+			if len(layout) >= i+2 && layout[i:i+2] == "HH" {
+				return layout[:i], token24HourTwoDigit, layout[i+2:]
+			}
+			if len(layout) >= i+1 && layout[i:i+1] == "H" {
+				return layout[:i], token24Hour, layout[i+1:]
+			}
+		case 'h': // h, hh
+			if len(layout) >= i+2 && layout[i:i+2] == "hh" {
+				return layout[:i], token12HourTwoDigit, layout[i+2:]
+			}
+			if len(layout) >= i+1 && layout[i:i+1] == "h" {
+				return layout[:i], token12Hour, layout[i+1:]
+			}
+		case 'm': // m, mm
+			if len(layout) >= i+2 && layout[i:i+2] == "mm" {
+				return layout[:i], tokenMinuteTwoDigit, layout[i+2:]
+			}
+			if len(layout) >= i+1 && layout[i:i+1] == "m" {
+				return layout[:i], tokenMinute, layout[i+1:]
+			}
+		case 's': // s, ss
+			if len(layout) >= i+2 && layout[i:i+2] == "ss" {
+				return layout[:i], tokenSecondTwoDigit, layout[i+2:]
+			}
+			if len(layout) >= i+1 && layout[i:i+1] == "s" {
+				return layout[:i], tokenSecond, layout[i+1:]
+			}
+		}
+	}
+	return layout, 0, ""
+}
+
+func parseStrictRFC3339Time(b []byte) (TimeOfDay, error) {
+	if len(b) < len(RFC3339Time) {
+		return TimeOfDay{}, &ParseError{Layout: RFC3339Time, Value: string(b)}
+	}
+
+	ok := true
+	parseUint := func(s []byte) (n int) {
+		for _, c := range s {
+			if !isDigit(c) {
+				ok = false
+				return 0
+			}
+			n = n*10 + int(c-'0')
+		}
+		return n
+	}
+
+	hour := parseUint(b[0:2])
+	min := parseUint(b[3:5])
+	sec := parseUint(b[6:8])
+	if !ok || b[2] != ':' || b[5] != ':' {
+		return TimeOfDay{}, &ParseError{Layout: RFC3339Time, Value: string(b)}
+	}
+
+	return NewTimeOfDay(hour, min, sec, 0)
+}
+
+// ParseTimeOfDay parses a formatted string and returns the time of day it represents.
+//
+//	a   am/pm  ante meridiem or post meridiem
+//	A   AM/PM  ante meridiem or post meridiem
+//	H    0-23  Two-digit hour, 24-hour clock
+//	HH  00-23  Hour, 24-hour clock
+//	h    1-12  Two-digit hour, 12-hour clock
+//	hh  01-12  Hour, 12-hour clock
+//	m    0-59  Minute
+//	mm  00-59  Minute, 2-digits
+//	s    0-59  Second
+//	ss  00-59  Second, 2-digits
+func ParseTimeOfDay(layout, value string) (TimeOfDay, error) {
+	var hour, min, sec int
+	var amSet, pmSet bool
+
+	originLayout, originValue := layout, value
+	var layoutElem, valueElem string
+	for {
+		prefix, token, suffix := nextTimeToken(layout)
+		if token == 0 {
+			break
+		}
+
+		layoutElem = layout[len(prefix) : len(layout)-len(suffix)]
+
+		layout = suffix
+		if len(value) < len(prefix) {
+			return TimeOfDay{}, &ParseError{Layout: originLayout, Value: originValue, LayoutElem: layoutElem, ValueElem: valueElem}
+		}
+		if value[:len(prefix)] != prefix {
+			return TimeOfDay{}, &ParseError{Layout: originLayout, Value: originValue, LayoutElem: prefix, ValueElem: value}
+		}
+		value = value[len(prefix):]
+
+		valueElem = value
+
+		var ok bool
+
+		switch token {
+		case tokenMidday:
+			var index int
+			index, value, ok = searchName([]string{"am", "pm"}, value)
+			switch index {
+			case 0:
+				amSet = true
+			case 1:
+				pmSet = true
+			}
+		case tokenMiddayUppercase:
+			var index int
+			index, value, ok = searchName([]string{"AM", "PM"}, value)
+			switch index {
+			case 0:
+				amSet = true
+			case 1:
+				pmSet = true
+			}
+		case token24Hour, token12Hour:
+			hour, value, ok = atoi(value, 1, 2)
+		case token24HourTwoDigit, token12HourTwoDigit:
+			hour, value, ok = atoi(value, 2, 2)
+		case tokenMinute:
+			min, value, ok = atoi(value, 1, 2)
+		case tokenMinuteTwoDigit:
+			min, value, ok = atoi(value, 2, 2)
+		case tokenSecond:
+			sec, value, ok = atoi(value, 1, 2)
+		case tokenSecondTwoDigit:
+			sec, value, ok = atoi(value, 2, 2)
+		}
+
+		if !ok {
+			return TimeOfDay{}, &ParseError{Layout: originLayout, Value: originValue, LayoutElem: layoutElem, ValueElem: valueElem}
+		}
+	}
+
+	if amSet && hour == 12 {
+		hour = 0
+	} else if pmSet && hour < 12 {
+		hour += 12
+	}
+
+	return NewTimeOfDay(hour, min, sec, 0)
+}
+
+func (t TimeOfDay) appendRFC3339(b []byte) []byte {
+	hour, min, sec, _ := nanosecondsToTime(t.n)
+
+	b = appendInt(b, hour, 2)
+	b = append(b, ':')
+	b = appendInt(b, min, 2)
+	b = append(b, ':')
+	b = appendInt(b, sec, 2)
+	return b
+}
+
+// hour12 returns the hour of 12-hour clock from 24-hour clock.
+func hour12(hour int) int {
+	hour12 := hour % 12
+	if hour12 == 0 {
+		hour12 = 12
+	}
+	return hour12
+}
+
+// midday returns the string of am/pm from 24-hour clock.
+func midday(hour int, am, pm string) string {
+	if hour < 12 {
+		return am
+	}
+	return pm
+}
+
+func (t TimeOfDay) format(layout string) string {
+	hour, min, sec, _ := nanosecondsToTime(t.n)
+	bytes := make([]byte, 0, len(layout)+10)
+
+	for {
+		prefix, token, suffix := nextTimeToken(layout)
+		bytes = append(bytes, prefix...)
+		if token == 0 {
+			break
+		}
+
+		layout = suffix
+
+		switch token {
+		case tokenMidday:
+			bytes = append(bytes, midday(hour, "am", "pm")...)
+		case tokenMiddayUppercase:
+			bytes = append(bytes, midday(hour, "AM", "PM")...)
+		case token24Hour:
+			bytes = appendInt(bytes, hour, 0)
+		case token24HourTwoDigit:
+			bytes = appendInt(bytes, hour, 2)
+		case token12Hour:
+			bytes = appendInt(bytes, hour12(hour), 0)
+		case token12HourTwoDigit:
+			bytes = appendInt(bytes, hour12(hour), 2)
+		case tokenMinute:
+			bytes = appendInt(bytes, min, 0)
+		case tokenMinuteTwoDigit:
+			bytes = appendInt(bytes, min, 2)
+		case tokenSecond:
+			bytes = appendInt(bytes, sec, 0)
+		case tokenSecondTwoDigit:
+			bytes = appendInt(bytes, sec, 2)
+		}
+	}
+
+	return string(bytes)
+}
+
+// Format returns a textual representation of the time of day.
+//
+//	a   am/pm  ante meridiem or post meridiem
+//	A   AM/PM  ante meridiem or post meridiem
+//	H    0-23  Two-digit hour, 24-hour clock
+//	HH  00-23  Hour, 24-hour clock
+//	h    1-12  Two-digit hour, 12-hour clock
+//	hh  01-12  Hour, 12-hour clock
+//	m    0-59  Minute
+//	mm  00-59  Minute, 2-digits
+//	s    0-59  Second
+//	ss  00-59  Second, 2-digits
+func (t TimeOfDay) Format(layout string) string {
+	switch layout {
+	case RFC3339Time:
+		b := make([]byte, 0, len(RFC3339Time))
+		b = t.appendRFC3339(b)
+		return string(b)
+	default:
+		return t.format(layout)
+	}
+}
+
+// String returns the textual representation of the time of day.
+func (t TimeOfDay) String() string {
+	return t.Format(RFC3339Time)
+}
+
+// GoString returns the Go syntax of the time of day.
+func (t TimeOfDay) GoString() string {
+	hour, min, sec, nsec := nanosecondsToTime(t.n)
+
+	bytes := make([]byte, 0, 32)
+
+	bytes = append(bytes, "timex.MustNewTimeOfDay("...)
+	bytes = appendInt(bytes, hour, 0)
+
+	bytes = append(bytes, ", "...)
+	bytes = appendInt(bytes, min, 0)
+
+	bytes = append(bytes, ", "...)
+	bytes = appendInt(bytes, sec, 0)
+
+	bytes = append(bytes, ", "...)
+	bytes = appendInt(bytes, nsec, 0)
+
+	bytes = append(bytes, ')')
+
+	return string(bytes)
+}
+
+// MarshalJSON implements the json.Marshaler interface.
+// The time of day is a quoted string in RFC 3339 format.
+func (t TimeOfDay) MarshalJSON() ([]byte, error) {
+	b := make([]byte, 0, len(RFC3339Time)+2)
+	b = append(b, '"')
+	b = t.appendRFC3339(b)
+	b = append(b, '"')
+	return b, nil
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+// The time of day is expected to be a quoted string in RFC 3339 format.
+func (t *TimeOfDay) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		return nil
+	}
+	if len(data) < 2 || data[0] != '"' || data[len(data)-1] != '"' {
+		return errors.New("TimeOfDay.UnmarshalJSON: input is not a JSON string")
+	}
+
+	var err error
+	*t, err = parseStrictRFC3339Time(data[1 : len(data)-1])
+	return err
+}
---
+++ time_of_day_format_test.go
@@ -0,0 +1,150 @@
+package timex_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/invzhi/timex"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseTimeOfDay(t *testing.T) {
+	tests := []struct {
+		layout string
+		value  string
+	}{
+		{timex.RFC3339Time, "15:04:05"},
+		{"HHmmss", "150405"},
+		{"H:m:s", "15:4:5"},
+		{"hh:mm:ssA", "03:04:05PM"},
+		{"hh:mm:ssa", "03:04:05pm"},
+		{"h:m:sa", "3:4:5pm"},
+		// Chinese
+		{"HH时m分s秒", "15时4分5秒"},
+	}
+
+	for _, tt := range tests {
+		timeOfDay, err := timex.ParseTimeOfDay(tt.layout, tt.value)
+		assert.NoError(t, err)
+
+		assert.Equal(t, 15, timeOfDay.Hour())
+		assert.Equal(t, 4, timeOfDay.Minute())
+		assert.Equal(t, 5, timeOfDay.Second())
+	}
+
+	t.Run("Format", func(t *testing.T) {
+		ts := []timex.TimeOfDay{
+			timex.MustNewTimeOfDay(0, 0, 0, 0),
+			timex.MustNewTimeOfDay(3, 4, 5, 0),
+			timex.MustNewTimeOfDay(15, 4, 5, 0),
+		}
+
+		for _, tt := range tests {
+			for _, t1 := range ts {
+				t2, err := timex.ParseTimeOfDay(tt.layout, t1.Format(tt.layout))
+				assert.NoError(t, err)
+
+				assert.Equal(t, t1, t2)
+			}
+		}
+	})
+}
+
+func TestParseTimeOfDayErrors(t *testing.T) {
+	tests := []struct {
+		layout    string
+		value     string
+		errString string
+	}{
+		{timex.RFC3339Time, "3:04:05", `parsing "3:04:05" as "HH:mm:ss": cannot parse "3:04:05" as "HH"`},
+		{timex.RFC3339Time, "015:04:05", `parsing "015:04:05" as "HH:mm:ss": cannot parse "5:04:05" as ":"`},
+		{" HH:mm:ss", "15:03:04", `parsing "15:03:04" as " HH:mm:ss": cannot parse "15:03:04" as " "`},
+		{" HH:mm:ss", "", `parsing "" as " HH:mm:ss": cannot parse "" as "HH"`},
+		{"hh:mm:ss", "a3:04:05", `parsing "a3:04:05" as "hh:mm:ss": cannot parse "a3:04:05" as "hh"`},
+		{"hh:mm:ss", "03:a4:05", `parsing "03:a4:05" as "hh:mm:ss": cannot parse "a4:05" as "mm"`},
+		{"hh:mm:ss", "03:04:a5", `parsing "03:04:a5" as "hh:mm:ss": cannot parse "a5" as "ss"`},
+	}
+
+	for _, tt := range tests {
+		_, err := timex.ParseTimeOfDay(tt.layout, tt.value)
+		assert.EqualError(t, err, tt.errString)
+	}
+}
+
+func FuzzParseTimeOfDay(f *testing.F) {
+	f.Add("HH:mm:ss", "15:04:05")
+	f.Add(" HH:mm:ss", "")
+	f.Fuzz(func(t *testing.T, layout, value string) {
+		assert.NotPanics(t, func() {
+			_, _ = timex.ParseTimeOfDay(layout, value)
+		})
+	})
+}
+
+func FuzzTimeOfDayFormat(f *testing.F) {
+	f.Add(0, "HH:mm:ss")
+	f.Add(0, " HH:mm:ss")
+	f.Fuzz(func(t *testing.T, n int, layout string) {
+		assert.NotPanics(t, func() {
+			_, timeOfDay := timex.TimeOfDay{}.AddDuration(time.Duration(n))
+			_ = timeOfDay.Format(layout)
+		})
+	})
+}
+
+func TestTimeOfDayString(t *testing.T) {
+	tests := []struct {
+		hour, min, sec, nsec int
+		str, goStr           string
+	}{
+		{0, 0, 0, 0, "00:00:00", "timex.MustNewTimeOfDay(0, 0, 0, 0)"},
+		{10, 0, 0, 0, "10:00:00", "timex.MustNewTimeOfDay(10, 0, 0, 0)"},
+		{12, 0, 0, 0, "12:00:00", "timex.MustNewTimeOfDay(12, 0, 0, 0)"},
+		{15, 04, 05, 0, "15:04:05", "timex.MustNewTimeOfDay(15, 4, 5, 0)"},
+		{23, 59, 59, 1e9 - 1, "23:59:59", "timex.MustNewTimeOfDay(23, 59, 59, 999999999)"},
+	}
+
+	for _, tt := range tests {
+		timeOfDay := timex.MustNewTimeOfDay(tt.hour, tt.min, tt.sec, tt.nsec)
+		assert.Equal(t, tt.str, timeOfDay.String())
+		assert.Equal(t, tt.goStr, timeOfDay.GoString())
+	}
+}
+
+func TestTimeOfDayMarshalJSON(t *testing.T) {
+	t1 := timex.MustNewTimeOfDay(15, 4, 5, 0)
+	bytes, err := t1.MarshalJSON()
+	assert.NoError(t, err)
+
+	var t2 timex.TimeOfDay
+	err = t2.UnmarshalJSON(bytes)
+	assert.NoError(t, err)
+	assert.Equal(t, t1, t2)
+
+	var t3 timex.TimeOfDay
+	err = t3.UnmarshalJSON([]byte("null"))
+	assert.NoError(t, err)
+	assert.True(t, t3.IsZero())
+}
+
+func TestTimeOfDayUnmarshalJSONError(t *testing.T) {
+	tests := []struct {
+		s         string
+		errString string
+	}{
+		{`15:04:05`, `TimeOfDay.UnmarshalJSON: input is not a JSON string`},
+		{`"3:04:05"`, `parsing "3:04:05" as "HH:mm:ss"`},
+		{`"-3:04:05"`, `parsing "-3:04:05" as "HH:mm:ss"`},
+		{`"100:04:05"`, `parsing "100:04:05" as "HH:mm:ss"`},
+		{`"12345:04:05"`, `parsing "12345:04:05" as "HH:mm:ss"`},
+		{`"15.04.05"`, `parsing "15.04.05" as "HH:mm:ss"`},
+		{`"15-04-05"`, `parsing "15-04-05" as "HH:mm:ss"`},
+		{`"HH.04.05"`, `parsing "HH.04.05" as "HH:mm:ss"`},
+	}
+
+	for _, tt := range tests {
+		var timeOfDay timex.TimeOfDay
+		err := timeOfDay.UnmarshalJSON([]byte(tt.s))
+		assert.EqualError(t, err, tt.errString)
+	}
+}
